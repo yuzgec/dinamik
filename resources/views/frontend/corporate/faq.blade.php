55757318812e8fb7f7d2f6f648485a2b54849d20
yuzgec <olcayy@gmail.com>
2022-11-26 07:35:54 +0300

    Fix

--- resources/views/frontend/corporate/faq.blade.php
+++ resources/views/frontend/corporate/faq.blade.php
@@ -12,8 +12,17 @@
         </div>
     </div>
 
-    <section class="contact-v2 pt-100 pb-100">
+    <section class="home2-faq-area pt-20 pb-30">
         <div class="container">
+
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="bixol-title-area text-center">
+                        <h3>SIKÇA SORULAN SORULAR</h3>
+                    </div>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-lg-12">
                     <div class="faq-content">
@@ -35,8 +44,6 @@
                         </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     </section>
